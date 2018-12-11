f941afcb705b69c6d20728a96692174fd3b29c0f
yoshi1220 <temari@themis.ocn.ne.jp>
2018-12-11 14:54:04 +0900

    initial commit

---
+++ app.rb
@@ -0,0 +1,39 @@
+require 'rubygems'
+require 'bundler'
+
+Bundler.require
+
+set :database, { adapter: "sqlite3", database: "contacts.sqlite3" }
+enable :sessions
+
+class Contact < ActiveRecord::Base
+  validates_presence_of :name
+  validates_presence_of :email
+end
+
+get '/' do
+  @now = Time.now
+  @contacts = Contact.all
+  @message = session.delete :message
+  erb :index
+end
+
+get '/contact_new' do
+  @contact = Contact.new
+  erb :contact_form
+end
+
+post '/contacts' do
+  p params
+
+  name = params[:name]
+
+  # DBに保存
+  @contact = Contact.new({name: name})
+  if @contact.save
+    session[:message] = "#{name}さんを作成しました。"
+    redirect '/'
+  else
+    erb :contact_form
+  end
+end
